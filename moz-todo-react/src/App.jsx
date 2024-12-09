ec10bf72ded032009c37ad539738dcc69a7ebdd4
jadegoesinsane <jkritzer1@ncstudents.niagaracollege.ca>
2024-12-09 15:01:00 -0500

    Set up React project with Vite and Tailwind CSS; remove old files and add new configurations

---
+++ moz-todo-react/src/App.jsx
@@ -0,0 +1,26 @@
+function App() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <section id="about">
+        <div>
+          <h2>About Me</h2>
+        </div>
+      </section >
+      <section id="resume">
+        <div>
+          <h2>Resume</h2>
+        </div>
+      </section >
+      <section id="portfolio">
+        <div>
+          <h2>Portfolio</h2>
+        </div>
+      </section >
+      
+    </div>
+  );
+}
+
+
+export default App;
---
+++ moz-todo-react/src/Header.jsx
@@ -0,0 +1,19 @@
+function Header() {
+    return (
+        <header className="p-4">
+            <nav className="flex justify-center gap-x-8">
+                <a href="/#about">
+                About
+                </a>
+                <a href="/#resume">
+                Resume
+                </a>
+                <a href="/#portfolio">
+                Portfolio
+                </a>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
